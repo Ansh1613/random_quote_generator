4b363acaed99b9de94bfa195db82dd1394f71f5a
Ansh1613 <anshrohilla1330@gmail.com>
2025-09-22 22:19:54 +0530

    added bgImage change functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import axios from "axios";
 
 function App(){
     const[advice, setAdvice] = useState('');
+    const[bgImage, setBgImage] = useState('');
 
     const fetchAdvice = async() => {
+        // Fetch a random background Advice from 
         try{
             const response = await axios.get('https://api.adviceslip.com/advice');
             setAdvice(response.data.slip.advice);
+
+            const imageURL = `https://picsum.photos/1200/800?random=${Math.floor(Math.random() * 1000)}`
+            setBgImage(imageURL);
         }
         catch(error){
             console.log(error);
@@ -20,7 +25,12 @@ function App(){
     }, []);
 
     return(
-        <div className="app">
+        <div 
+        className="app"
+        style={bgImage ? {
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${bgImage})`,
+            transition: 'background-image 0.5s ease-in-out'
+        } : {}}>
             <div className="card">
                 <h1 className="heading">{advice}</h1>
                 <button className="button" onClick={fetchAdvice}>
